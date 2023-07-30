7dad63bd9d650c190c251bca64c341a0dac1bec8
SGontcho <satya.gontcho@gmail.com>
2023-07-30 11:09:27 -0700

    cleaned up query_database.py

--- src/mirror_temperature/query_database.py
+++ src/mirror_temperature/query_database.py
@@ -69,9 +69,9 @@ def _query_environmentmonitor_tower(conn):
                     "wind_direction"
                 FROM
                     "environmentmonitor_tower"
-                LIMIT 10
                 """
                 # WHERE time_recorded < X AND time_recorded > y
+                # LIMIT 10
             )
             rows = cur.fetchall()
             data = load(rows=rows, columns=[
@@ -103,9 +103,9 @@ def _query_environmentmonitor_dome(conn):
                     "dome_floor_s"
                 FROM
                     "environmentmonitor_dome"
-                LIMIT 10
                 """
                 # WHERE time_recorded < X AND time_recorded > y
+                # LIMIT 10
             )
             rows = cur.fetchall()
             data = load(rows=rows, columns=[
@@ -139,9 +139,9 @@ def _query_environmentmonitor_telescope(conn):
                     "air_dewpoint"
                 FROM
                     "environmentmonitor_telescope"
-                LIMIT 10
                 """
                 # WHERE time_recorded < X AND time_recorded > y
+                # LIMIT 10
             )
             rows = cur.fetchall()
             data = load(rows=rows, columns=[
@@ -164,18 +164,18 @@ def _query_etc_seeing(conn):
                 """
                 SELECT
                     "time_recorded",
-                    "etc_seeing",
+                    "etc_seeing", 
                     "seeing"
                 FROM
                     "etc_seeing"
-                LIMIT 10
                 """
                 # WHERE time_recorded < X AND time_recorded > y
+                # LIMIT 10
             )
             rows = cur.fetchall()
             data = load(rows=rows, columns=[
                 "time_recorded",
-                "etc_seeing",
+                "etc_seeing", 
                 "seeing",
             ])
     return data
@@ -188,20 +188,20 @@ def _query_etc_telemetry(conn):
                 """
                 SELECT
                     "time_recorded",
-                    "seeing",
-                    "transparency",
+                    "seeing", 
+                    "transparency", 
                     "skylevel"
                 FROM
                     "etc_telemetry"
-                LIMIT 10
                 """
                 # WHERE time_recorded < X AND time_recorded > y
+                # LIMIT 10
             )
             rows = cur.fetchall()
             data = load(rows=rows, columns=[
                 "time_recorded",
-                "seeing",
-                "transparency",
+                "seeing", 
+                "transparency", 
                 "skylevel",
             ])
     return data
@@ -214,18 +214,18 @@ def _query_tcs_info(conn):
                 """
                 SELECT
                     "time_recorded",
-                    "mirror_ready",
+                    "mirror_ready", 
                     "airmass"
                 FROM
                     "tcs_info"
-                LIMIT 10
                 """
                 # WHERE time_recorded < X AND time_recorded > y
+                # LIMIT 10
             )
             rows = cur.fetchall()
             data = load(rows=rows, columns=[
                 "time_recorded",
-                "mirror_ready",
+                "mirror_ready", 
                 "airmass",
             ])
     return data
@@ -243,38 +243,8 @@ def retrieve_and_store_data_from_database(settings: Settings) -> None:
     None
     """
 
-## FIRST (messier version) OF QUERYING
- # Data table names and respective columns of interest
- #   labels["environmentmonitor_tower"] = [
- #       "time_recorded",
- #       "temperature",
- #       "pressure",
- #       "humidity",
- #       "wind_speed",
- #       "wind_direction",
- #   labels["environmentmonitor_dome"] = [
- #       "time_recorded",
- #       "dome_left_upper",
- #       "dome_left_lower",
- #       "dome_right_upper",
- #       "dome_right_lower",
- #       "dome_back_upper",
- #       "dome_back_lower",
- #       "dome_floor_ne",
- #       "dome_floor_nw",
- #       "dome_floor_s",
- #   labels["environmentmonitor_telescope"] = [
- #       "time_recorded",
- #       "mirror_avg_temp",
- #       "mirror_desired_temp",
- #       "mirror_temp",
- #       "mirror_cooling",
- #       "air_temp",
- #       "air_flow",
- #       "air_dewpoint",
-
     # Establish connection to server and begin SQL queries using labels shown above.
-    pd.DataFrame()
+    data = pd.DataFrame()
     conn = psycopg2.connect(
         host=settings.desi_db_host,
         port=settings.desi_db_port,
@@ -282,22 +252,16 @@ def retrieve_and_store_data_from_database(settings: Settings) -> None:
         user=settings.desi_db_username,
         password=settings.desi_db_password,
     )
-
+    
     table_queries = {
-        "test_environmentmonitor_tower": _query_environmentmonitor_tower,
-        "test_environmentmonitor_dome": _query_environmentmonitor_dome,
-        "test_environmentmonitor_telescope": _query_environmentmonitor_telescope,
-        "test_etc_seeing": _query_etc_seeing,
-        "test_etc_telemetry": _query_etc_telemetry,
-        "test_tcs_info": _query_tcs_info,
+        "environmentmonitor_tower": _query_environmentmonitor_tower,
+        "environmentmonitor_dome": _query_environmentmonitor_dome,
+        "environmentmonitor_telescope": _query_environmentmonitor_telescope,
+        "etc_seeing": _query_etc_seeing,
+        "etc_telemetry": _query_etc_telemetry,    
+        "tcs_info": _query_tcs_info,    
     }
-
+    
     for table_name, query in tqdm(table_queries.items(), desc="hello"):
         rv = query(conn)
         save(settings.output_folder, rv, table=table_name)
-
-    #for table, columns in tqdm(labels.items(), desc="Querying database tables..."):
-     #   with conn:
-      #      with conn.cursor() as cur:
-       #         # TODO: Selecting this way is bad practice.
-        #        # Write explicit selects for each table without any cleverness.
